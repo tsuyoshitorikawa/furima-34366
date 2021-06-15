66285c44e7fd0f74a4962cbfd2b729b0c3f44ba0
tsuyoshitorikawa <gangtulichuan@gmail.com>
2021-06-15 17:01:27 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or request_with_http_basic do |username, password|
+      userame == 'admin' && password == '2222'
+    end
+  end
 end
