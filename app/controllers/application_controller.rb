6847800411f7f0360a3d97956c8d86d5565ffdd9
tsuyoshitorikawa <gangtulichuan@gmail.com>
2021-06-27 19:23:05 +0900

    新規登録/ログイン/ログアウトにおけるパスの記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,21 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :encrypted_password, :first_mane, :last_name, :first_name_kana, :last_name_kana, :birthbay])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
+  end
+  
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
   end
 end
 
