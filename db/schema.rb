5678a9d9a1ab56699a1a58b0fc74e03c3665e01a
tsuyoshitorikawa <gangtulichuan@gmail.com>
2021-06-29 18:23:03 +0900

    カラム/テーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,18 @@
 ActiveRecord::Schema.define(version: 2021_06_25_122739) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "product_name", null: false
+    t.text "description_name", null: false
+    t.integer "price", null: false
+    t.integer "area_id", null: false
+    t.integer "burden_id", null: false
+    t.integer "delivery_state_id", null: false
+    t.integer "category_id", null: false
+    t.integer "day_to_ship_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -35,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_06_25_122739) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
