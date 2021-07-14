13a5b6f8cd90736b8b81f62df06b5497716f4e3f
tsuyoshitorikawa <gangtulichuan@gmail.com>
2021-07-14 17:38:15 +0900

    商品詳細機能の改良

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  # before_action :authenticate_user!, except: [:index]
   
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -22,20 +22,20 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def edit
-    @item = Item.find(params[:id])
-  end
+  # def edit
+  #   @item = Item.find(params[:id])
+  # end
 
-  def update
-    @item = Item.find(params[:id])
-    @item.update(@item_params)
-    redirect_to item_path
-  end
+  # def update
+  #   @item = Item.find(params[:id])
+  #   @item.update(@item_params)
+  #   redirect_to item_path
+  # end
 
-  def destroy
-    @item = Item.find(params[:id])
-    @item.destroy
-  end
+  # def destroy
+  #   @item = Item.find(params[:id])
+  #   @item.destroy
+  # end
 
   def item_params
     params.require(:item).permit(:image, :product_name, :description_name, :price, :area_id, :burden_id, :delivery_state_id, :category_id, :day_to_ship_id).merge(user_id: current_user.id)
