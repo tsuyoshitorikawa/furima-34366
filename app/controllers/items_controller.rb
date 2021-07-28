107bc693640ae9c20b0803722ec268f14bf2a1af
tsuyoshitorikawa <gangtulichuan@gmail.com>
2021-07-28 18:01:12 +0900

    商品購入機能の編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :show, :update, :destroy]
-  before_action :present?, omly: [:edit, :create]
+  before_action :present?, omly: [:edit, :update]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
